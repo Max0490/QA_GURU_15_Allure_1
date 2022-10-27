20a7ab91d175fd06ba2b765c303c0737bbd1e054
USER <seagate90@mail.ru>
2022-10-27 20:04:39 +0400

    Add classes

---
+++ src/test/java/qa/guru/allure/LabelSteps.java
@@ -0,0 +1,43 @@
+package qa.guru.allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class LabelSteps {
+
+    @Test
+    @Feature("Issue в репозитории")
+    @Story("Создание Issue")
+    @Owner("eroshenkoam")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "Testing", url = "https://testing.github.com")
+    @DisplayName("Создание Issue для авторизованного пользователя")
+    public void testStaticLabels() {
+
+    }
+
+    @Test
+    public void testDynamicLabels() {
+        Allure.getLifecycle().updateTestCase(
+                t -> t.setName("Создание Issue для авторизованного пользователя")
+        );
+        Allure.feature("Issue в репозитории");
+        Allure.story("Создание Issue");
+        Allure.label("owner", "eroshenkoam");
+        Allure.label("severity", SeverityLevel.CRITICAL.value());
+        Allure.link("Testing", "https://testing.github.com");
+    }
+
+
+}
--- src/test/java/qa/guru/allure/WebSteps.java
+++ src/test/java/qa/guru/allure/WebSteps.java
@@ -41,6 +41,7 @@ public class WebSteps {
         $(withText("#" + issue)).should(Condition.exist);
     }
 
+
     @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
     public byte[] takeScreenshot() {
         return ((TakesScreenshot)WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
